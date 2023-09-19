bfd1d0f066a6b0f673e68a48393494410fe8fba9
Jonnie <jtech2096@gmail.com>
2023-09-19 11:18:33 +0100

    project init + firebase setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Login from "./Components/Login";
+
+function App() {
+  return (
+    <section>
+      <div>Hello</div>
+      <Login />
+    </section>
+  );
+}
+
+export default App;
---
+++ src/Components/Login.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+
+function Login() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      setIsLoggedIn(true);
+      //   alert("Successfully logged in");
+      // User successfully logged in
+    } catch (error) {
+      console.log("Login error:", error.message);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
